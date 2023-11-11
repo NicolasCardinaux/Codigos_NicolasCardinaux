342f67615dff30015554b06ff1c6a7f7c6b37429
NicolasCardinaux <127860186+NicolasCardinaux@users.noreply.github.com>
2023-11-10 21:34:38 -0300

    Update Ejercicio15.py

--- TP6/EjerciciosGraf/Ejercicio15.py
+++ TP6/EjerciciosGraf/Ejercicio15.py
@@ -1,4 +1,4 @@
-''' Se requiere implementar un grafo para almacenar las siete maravillas arquitectónicas moder-
+'''Se requiere implementar un grafo para almacenar las siete maravillas arquitectónicas moder-
 nas y naturales del mundo, para lo cual se deben tener en cuenta las siguientes actividades:
 
 a. de cada una de las maravillas se conoce su name, país de ubicación (puede ser más de
@@ -12,34 +12,40 @@ f. deberá utilizar un grafo no dirigido.'''
 from grafo import Grafo01
 from random import randint
 
+# Se requiere implementar un grafo para almacenar las siete maravillas arquitectónicas moder-
+# nas y naturales del mundo, para lo cual se deben tener en cuenta las siguientes actividades:
+
+
 class Maravilla:
     def __init__(self, name, country, types):
         self.name = name
         self.country = country if isinstance(country, list) else [country]
         self.types = types
 
-    def add_pais(self, pais):
-        self.country.append(pais)
-
     def __str__(self):
         return f"Nombre: {self.name}, Países: {', '.join(self.country)}, Tipo: {self.types}"
 
+# A. de cada una de las maravillas se conoce su nombre, país de ubicación (puede ser más de
+# uno en las naturales) y tipo (natural o arquitectónica);
 maravillas_naturales = [
-    Maravilla("Cataratas del Iguazú", ["Argentina", "Brasil"], "natural"),
+    Maravilla("Aurora Boreal", "Varios lugares del Ártico", "natural"),
     Maravilla("Gran Cañón", "Estados Unidos", "natural"),
     Maravilla("Gran Barrera de Coral", "Australia", "natural"),
-    Maravilla("Aurora Boreal", ["Varios lugares del Ártico"], "natural"),
-    Maravilla("Monte Everest", ["Nepal", "Tíbet"], "natural"),
-    Maravilla("Parque Nacional de Yellowstone", "Estados Unidos", "natural")
+    Maravilla("Parque Nacional de Yellowstone", "Estados Unidos", "natural"),
+    Maravilla("Cataratas del Iguazú", "Argentina", "natural"),
+    Maravilla("Monte Everest", "Nepal", "natural"),
 ]
+
+# Ahora, puedes utilizar estas maravillas naturales con las mismas distancias aleatorias en el grafo
+
 
 maravillas_arquitectonicas = [
     Maravilla("Gran Pirámide de Guiza", "Egipto", "arquitectónica"),
-    Maravilla("Gran Muralla China", "China", "arquitectónica"),
     Maravilla("Taj Mahal", "India", "arquitectónica"),
-    Maravilla("Estatua de la Libertad", "Estados Unidos", "arquitectónica"),
+    Maravilla("Gran Muralla China", "China", "arquitectónica"),
     Maravilla("Coliseo Romano", "Italia", "arquitectónica"),
-    Maravilla("Ciudad de Petra", "Jordania", "arquitectónica")
+    Maravilla("Estatua de la Libertad", "Estados Unidos", "arquitectónica"),
+    Maravilla("Teatro Colón", "Argentina", "arquitectónica"),
 ]
 
 dic = {}
@@ -48,99 +54,86 @@ for i in range(6):
     dic[maravillas_arquitectonicas[i].name] = i
     dic[maravillas_naturales[i].name] = i
 
-new_graph = Grafo01(dirigido=False)
+# F. deberá utilizar un grafo no dirigido.
+mi_grafo = Grafo01(dirigido=False)
 
 for i in maravillas_arquitectonicas:
-    new_graph.insert_vertice(i.name)
+    mi_grafo.insert_vertice(i.name)
 
 for i in maravillas_naturales:
-    new_graph.insert_vertice(i.name)
+    mi_grafo.insert_vertice(i.name)
 
-# Punto a: Conecta las maravillas arquitectónicas entre sí con distancias aleatorias
+# conecto las aristas y chequeo que no se repitan     
+# B. cada una debe estar relacionada con las otras seis de su tipo, para lo que se debe almacenar
+# la distancia que las separa;
 for i in maravillas_arquitectonicas:
-    positiona = new_graph.search_vertice(i.name)
-    pointa = new_graph.get_element_by_index(positiona)
+    valor_i = mi_grafo.search_vertice(i.name)
+    posicion_uno = mi_grafo.get_element_by_index(valor_i)
     for j in maravillas_arquitectonicas:
-        positionb = new_graph.search_vertice(j.name)
-        pointb = new_graph.get_element_by_index(positionb)
-        checker = new_graph.is_adyacent(pointa[0], pointb[0])
-        if pointa != pointb and checker == False:
-            value = randint(100, 5000)
-            new_graph.insert_arist(pointa[0], pointb[0], value)
+        valor_j = mi_grafo.search_vertice(j.name)
+        posicion_dos = mi_grafo.get_element_by_index(valor_j)
+        if posicion_uno != posicion_dos:
+            if not mi_grafo.is_adyacent(posicion_uno[0], posicion_dos[0]):
+                value = randint(100, 5000)
+                mi_grafo.insert_arist(posicion_uno[0], posicion_dos[0], value)
+         
 
-# Punto b: Conecta las maravillas naturales entre sí con distancias aleatorias
 for i in maravillas_naturales:
-    positiona = new_graph.search_vertice(i.name)
-    pointa = new_graph.get_element_by_index(positiona)
+    valor_i = mi_grafo.search_vertice(i.name)
+    posicion_uno = mi_grafo.get_element_by_index(valor_i)
     for j in maravillas_naturales:
-        positionb = new_graph.search_vertice(j.name)
-        pointb = new_graph.get_element_by_index(positionb)
-        checker = new_graph.is_adyacent(pointa[0], pointb[0])
-        if pointa != pointb and checker == False:
-            value = randint(100, 5000)
-            new_graph.insert_arist(pointa[0], pointb[0], value)
+        valor_j = mi_grafo.search_vertice(j.name)
+        posicion_dos = mi_grafo.get_element_by_index(valor_j)
+        if posicion_uno != posicion_dos:
+            if not mi_grafo.is_adyacent(posicion_uno[0], posicion_dos[0]):
+                value = randint(100, 5000)
+                mi_grafo.insert_arist(posicion_uno[0], posicion_dos[0], value)
+        
+print('--------------------------------------------------------------------------------')
 
-# Punto c: Encuentra países que tienen ambos tipos de maravillas y los imprime
-countries = []
-for i in maravillas_naturales:
-    positiona = new_graph.search_vertice(i.name)
-    pointa = new_graph.get_element_by_index(positiona)
-    for j in maravillas_arquitectonicas:
-        positionb = new_graph.search_vertice(j.name)
-        pointb = new_graph.get_element_by_index(positionb)
-        index_A = dic[pointa[0]]
-        index_B = dic[pointb[0]]
-        if maravillas_arquitectonicas[index_B].country[0] in maravillas_naturales[index_A].country:
-            if (maravillas_arquitectonicas[index_B].country in countries) == False:
-                countries.append(maravillas_arquitectonicas[index_B].country)
-
-# Imprime los países que tienen ambos tipos de maravillas
-for i in countries:
-    print(f"Punto c: {i[0]} posee los 2 tipos de maravilla")
-
-# Punto d: Encuentra países con más de una maravilla del mismo tipo (naturales)
-nature_2 = []
-for i in maravillas_naturales:
-    positiona = new_graph.search_vertice(i.name)
-    pointa = new_graph.get_element_by_index(positiona)
-    for j in maravillas_naturales:
-        positionb = new_graph.search_vertice(j.name)
-        pointb = new_graph.get_element_by_index(positionb)
-        index_A = dic[pointa[0]]
-        index_B = dic[pointb[0]]
-        for k in maravillas_naturales[index_B].country:
-            for l in maravillas_naturales[index_A].country:
-                if k == l and pointb[0] != pointa[0]:
-                    if (k in nature_2) == False:
-                        nature_2.append(k)
-
-# Imprime los países con 2 maravillas naturales del mismo tipo
-for i in nature_2:
-    print(f"Punto d: {i} posee 2 maravillas(naturaleza) del mismo tipo")
-
-# Punto e: Encuentra países con más de una maravilla del mismo tipo (arquitectónicas)
-non_nature_2 = []
-for i in maravillas_arquitectonicas:
-    positiona = new_graph.search_vertice(i.name)
-    pointa = new_graph.get_element_by_index(positiona)
-    for j in maravillas_arquitectonicas:
-        positionb = new_graph.search_vertice(j.name)
-        pointb = new_graph.get_element_by_index(positionb)
-        index_A = dic[pointa[0]]
-        index_B = dic[pointb[0]]
-        for k in maravillas_arquitectonicas[index_B].country:
-            for l in maravillas_arquitectonicas[index_A].country:
-                if k == l and pointb[0] != pointa[0]:
-                    if (k in non_nature_2) == False:
-                        non_nature_2.append(k)
-
-# Imprime los países con 2 maravillas arquitectónicas del mismo tipo
-for i in non_nature_2:
-    print(f"Punto e: {i} posee 2 maravillas(arquitectura) del mismo tipo")
-
-# Punto f: Encuentra y muestra el árbol de expansión mínimo (MST) para cada tipo de maravilla
-bosque = new_graph.kruskal()
-for arbol in bosque:
-    print('Punto f: Árbol de expansión mínimo (MST)')
-    for nodo in arbol.split(';'):
+# C. hallar el árbol de expansión mínimo de cada tipo de las maravillas;
+print('Punto C')
+bosque = mi_grafo.kruskal()
+for i, arbol in enumerate(bosque, start=1):
+    print(f"Árbol de expansión mínima {i} para el tipo de maravilla:")
+    for nodo in arbol.split(";"):
         print(nodo)
+    print()
+
+print('--------------------------------------------------------------------------------')       
+#D. determinar si existen países que dispongan de maravillas arquitectónicas y naturales;
+print('Punto D')
+
+countries_with_both_types = set()
+
+for maravilla in maravillas_naturales:
+    for país in maravilla.country:
+        for otra_maravilla in maravillas_arquitectonicas:
+            if país in otra_maravilla.country:
+                countries_with_both_types.add(país)
+
+if countries_with_both_types:
+    print("Países que disponen de maravillas arquitectónicas y naturales:")
+    for país in countries_with_both_types:
+        print(país)
+
+print('--------------------------------------------------------------------------------')
+
+# E. determinar si algún país tiene más de una maravilla del mismo tipo;
+print('Punto E')
+nature_countries = set()
+non_nature_countries = set()
+
+for maravilla in maravillas_arquitectonicas + maravillas_naturales:
+    for país in maravilla.country:
+        if maravilla.types == "natural":
+            if país in nature_countries:
+                print(f"{país} tiene más de una maravilla natural")
+            else:
+                nature_countries.add(país)
+        elif maravilla.types == "arquitectónica":
+            if país in non_nature_countries:
+                print(f"{país} tiene más de una maravilla arquitectónica")
+            else:
+                non_nature_countries.add(país)
+print('--------------------------------------------------------------------------------') 
